8bd41266da2ff20c04f2e0756aff81dd172eb0d3
Jose Patron <patronalayola55@gmail.com>
2019-02-11 20:58:19 -0600

    segundo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var ceentral = require ("./clonando_equipo");
+
+const personaje1 = "Diana";
+const personaje2 = "Diego";
+const personaje3 = "Jose";
+const hora_llegada = 7;
+const hora_descanso = 8;
+const hora_salida = 9;
+
+console.log (ceentral.llegada(hora_llegada,hora_descanso, hora_salida));
