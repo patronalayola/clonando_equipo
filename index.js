7f40f3f16b956f1dd56da431157b71a8ba186756
Jose Patron <patronalayola55@gmail.com>
2019-02-12 21:46:11 -0600

    ayudaaa

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var ceentral = require ("./historia3");
+//VARIABLES
+var ceentral = require ("./final");
 const fs = require("fs");
+
+//VARIABLES DE GENERACION
 const personaje1 = "Diana";
 const personaje2 = "Diego";
 const personaje3 = "Jose";
@@ -8,4 +11,31 @@ const hora_descanso = 8;
 const hora_salida = 10;
 const edad = Math.floor(Math.random() * 100);
 
-console.log (ceentral.historia(personaje1, personaje2, personaje3,hora_llegada,hora_descanso, hora_salida));
+fs.readFile("personajes.txt","utf-8",function(err,contenido){
+  if(err){
+    throw err;
+  }
+
+const personajes = contenido.split(",");
+const numeroPersonaje = Math.floor(Math.random() * personajes.length);
+const personajeRandom = personajes[numeroPersonaje];
+
+const historiaCompleta = juntarHistoria(personajeRandom);
+
+console.log(historiaCompleta);
+
+});
+
+fs.writeFile("historia.txt", historiaCompleta, function(err){
+  if(err){
+    throw err;
+  }
+});
+
+function juntarHistoria(personaje){
+  const introHistoria = intro.historia(personaje, lugar);
+  const finalHistoria = final.historia(personaje, lugar);
+  const historiaCompleta = introHistoria + finalHistoria;
+  return historiaCompleta;
+
+}
